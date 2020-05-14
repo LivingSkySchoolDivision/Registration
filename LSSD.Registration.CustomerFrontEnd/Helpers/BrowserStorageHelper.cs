86ad72190cce792e847284cfd77dab302eaec30f
Mark Strendin <mark.strendin@gmail.com>
2020-05-14 15:49:23 -0600

    Run storage through a helper that can handle errors better. Tried two layers of dependency injection, and this didn't work. Static class for now because I know it works.

---
+++ LSSD.Registration.CustomerFrontEnd/Helpers/BrowserStorageHelper.cs
@@ -0,0 +1,33 @@
+﻿using Blazored.LocalStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSSD.Registration.CustomerFrontEnd.Helpers
+{
+    public static class BrowserStorageHelper
+    {
+        public static async Task<T> GetItem<T>(ILocalStorageService StorageService, string Key) where  T : new()
+        {
+            try
+            {
+                return await StorageService.GetItemAsync<T>(Key);
+            }
+            catch { }
+
+            return new T();
+        }
+
+        public static async Task SetItem<T>(ILocalStorageService StorageService, string Key, T Object)
+        {
+            await StorageService.SetItemAsync(Key, Object);
+        }
+
+        public static async Task Clear(ILocalStorageService StorageService)
+        {
+            await StorageService.ClearAsync();
+        }
+
+    }
+}
