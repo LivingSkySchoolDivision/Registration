e4a7f5ea1867c0c02bc65252854a5908d53062da
Mark Strendin <mark.strendin@gmail.com>
2020-05-14 22:21:57 -0600

    Rename "Sask Health Card Number" to match provincial language

--- LSSD.Registration.Model/Student.cs
+++ LSSD.Registration.Model/Student.cs
@@ -27,7 +27,7 @@ namespace LSSD.Registration.Model
         public Address MailingAddress { get; set; }
         public string LandDescription { get; set; }
         public string PreviousSchools { get; set; }
-        public string SaskHealthNumber { get; set; }
+        public string HealthServicesNumber { get; set; }
         public string MedicalNotes { get; set; }
 
         public Student()
