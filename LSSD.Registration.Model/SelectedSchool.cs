f2e02417caaa14f27f801da364504817686a58a9
Mark Strendin <mark.strendin@gmail.com>
2020-05-22 10:13:55 -0600

    Store the selected school in a better format. Fixes #20

---
+++ LSSD.Registration.Model/SelectedSchool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSSD.Registration.Model
+{
+    public class SelectedSchool
+    {
+        public int Id { get; set; }
+        public string DAN { get; set; }
+        public string Name { get; set; }
+
+
+        public static SelectedSchool FromSchool(School school)
+        {
+            if (school != null)
+            {
+                return new SelectedSchool()
+                {
+                    Id = school.Id,
+                    DAN = school.DAN,
+                    Name = school.Name
+                };
+            } else
+            {
+                return new SelectedSchool();
+            }
+        }
+    }
+}
