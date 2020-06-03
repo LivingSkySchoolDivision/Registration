c283f45c96c534a8208958ffcf84845dab499cab
Mark Strendin <mark.strendin@gmail.com>
2020-06-03 14:35:03 -0600

    Add debug console to help get some data in

---
+++ LSSD.Registration.DebugConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LSSD.Registration.DebugConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LSSD.Registration.PublicAPI/Controllers/SchoolController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LSSD.Registration.Data;
+using LSSD.Registration.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LSSD.Registration.PublicAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SchoolController : ControllerBase
+    {
+        private readonly IRepository<School> _repository;
+
+        public SchoolController(IRepository<School> SchoolRepository)
+        {
+            this._repository = SchoolRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<School> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public School Get(string guid)
+        {
+            return _repository.GetById(guid);
+        }
+
+
+
+
+
+        [HttpPost]
+        public void Post([FromBody] School value)
+        {
+        }
+
+        // PUT api/<SchoolController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] School value)
+        {
+        }
+
+    }
+}
