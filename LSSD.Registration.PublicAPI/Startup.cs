67b8739dfc41552e0ae923d36e66f8b081c24d46
Mark Strendin <mark.strendin@gmail.com>
2020-06-03 14:22:51 -0600

    Lets start by building out the school API

--- LSSD.Registration.PublicAPI/Startup.cs
+++ LSSD.Registration.PublicAPI/Startup.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using LSSD.Registration.Data;
-using LSSD.Registration.Model.Forms;
+using LSSD.Registration.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace LSSD.Registration.PublicAPI
 {
@@ -35,7 +28,7 @@ namespace LSSD.Registration.PublicAPI
             });
 
             services.AddSingleton<MongoDbConnection>();
-            services.AddSingleton<MongoRepository<PreKRegistrationFormSubmission>>();
+            services.AddSingleton<MongoRepository<School>>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
