a99d4ec8b674b55303d8b5a978b31cf67dc4b825
Mark Strendin <mark.strendin@gmail.com>
2020-06-03 19:45:46 -0600

    Store interface with model - see previous commit

---
+++ LSSD.Registration.Model/IRegistrationRepository.cs
@@ -0,0 +1,19 @@
+﻿using LSSD.Registration.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace LSSD.Registration.Model
+{
+    public interface IRegistrationRepository<T> where T : IGUIDable
+    {
+        Guid Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        IList<T> Find(Expression<Func<T, bool>> predicate);
+        IList<T> GetAll();
+        T GetById(Guid id);
+        T GetById(string id);
+    }
+}
