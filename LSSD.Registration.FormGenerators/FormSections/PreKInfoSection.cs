14348e36a5ce934b89efd572d3abb361f5066fc4
Mark Strendin <mark.strendin@gmail.com>
2020-06-13 20:48:17 -0600

    More work on getting PreK fields in the PreK page

--- LSSD.Registration.FormGenerators/FormSections/PreKInfoSection.cs
+++ LSSD.Registration.FormGenerators/FormSections/PreKInfoSection.cs
@@ -13,49 +13,90 @@ namespace LSSD.Registration.FormGenerators.FormSections
         {
             List<OpenXmlElement> sectionParts = new List<OpenXmlElement>();
 
-            sectionParts.AddRange(assistanceSection(PreKInfo));
+            sectionParts.Add(ColumnHelper.SetPreviousSectionToColumns(1));
+            
+            sectionParts.Add(new Paragraph());
+
+            sectionParts.Add(
+                TableHelper.MakeTable(new List<KeyValuePair<string, bool>>() {
+                    new KeyValuePair<string, bool>("Single parent or frequent parent absence", PreKInfo.OnlyOneParentInHome),
+                    new KeyValuePair<string, bool>("Lack of family support system", PreKInfo.NoFamilySupportSystem),
+                    new KeyValuePair<string, bool>("Low income family in financial need", PreKInfo.LowIncomeFamily),
+                    new KeyValuePair<string, bool>("Primary caregiver less than high school education", PreKInfo.PrimaryCaregiverLessThanHighSchoolEducation),
+                    new KeyValuePair<string, bool>("Child lives with teen parent", PreKInfo.TeenParent),
+                    new KeyValuePair<string, bool>("Child in foster care", PreKInfo.InFosterCare),
+                    new KeyValuePair<string, bool>("Little opportunity to interact with other same age", PreKInfo.LittleOpportunityToInteractWithSameAge),
+                    new KeyValuePair<string, bool>("Can use bathroom by self", PreKInfo.CanUseBathroomAlone),
+                    new KeyValuePair<string, bool>("Bathroom training in progress", PreKInfo.PottyTrainingInProgress),
+                })
+            );
+
+            sectionParts.Add(ColumnHelper.ColumnBreak());
+
+            sectionParts.Add(
+                TableHelper.MakeTable(new List<KeyValuePair<string, bool>>() {
+                    new KeyValuePair<string, bool>("Speech difficulties", PreKInfo.SpeechDifficulties),
+                    new KeyValuePair<string, bool>("Language difficulties", PreKInfo.LanguageDifficulties),
+                    new KeyValuePair<string, bool>("Gross Motor difficulties", PreKInfo.GrossMotorDifficulties),
+                    new KeyValuePair<string, bool>("Fine Motor difficulties", PreKInfo.FineMotorDifficulties)
+                })
+            );
+            sectionParts.Add(new Table(
+                LSSDTableStyles.Borders(),
+                LSSDTableStyles.Margins(),
+                new TableRow(
+                    new TableCell(
+                        new TableCellProperties(
+                            new GridSpan() { Val = 2 }
+                        ),
+                        ParagraphHelper.Paragraph("Other difficulties:", "Field Label"),
+                        ParagraphHelper.Paragraph(PreKInfo.OtherDifficulties, "Field Value")
+                    )
+                )
+            ));
+
+            sectionParts.Add(ColumnHelper.SetPreviousSectionToColumns(2, 200));
 
             sectionParts.Add(new Paragraph());
-            return sectionParts;
-        }
+            
+            sectionParts.Add(
+                TableHelper.MakeTable(new List<KeyValuePair<string, string>>() {
+                    new KeyValuePair<string, string>("Social, emotional, or behavior issues:", PreKInfo.SocialEmotionalOrBehaviourIssues),
+                    new KeyValuePair<string, string>("Traumatic experience within or impacting the family/child:", PreKInfo.TraumaticExperiences),
+                    new KeyValuePair<string, string>("Health care crisis impacting child or family:", PreKInfo.HealthcareCrisis),
+                    new KeyValuePair<string, string>("Referred by agency or agencies:", PreKInfo.ReferredByOtherAgency),
+                    new KeyValuePair<string, string>("Custody concerns:", PreKInfo.CustodyConcerns),
+                    new KeyValuePair<string, string>("Medical concerns:", PreKInfo.MedicalConcerns),
+                    new KeyValuePair<string, string>("Other concerns:", PreKInfo.OtherConcerns),
+                })
+            );
 
+            sectionParts.Add(ColumnHelper.SetPreviousSectionToColumns(1, 200));
 
-        private static IEnumerable<OpenXmlElement> assistanceSection(PreKInfo PreKInfo)
-        {
-            List<OpenXmlElement> returnMe = new List<OpenXmlElement>();
-            //returnMe.Add(new SectionProperties());
+            sectionParts.Add(ParagraphHelper.WhiteSpace());
+            sectionParts.Add(ParagraphHelper.Paragraph("Child receives supports from the following:", "Field Label"));
+          
+            sectionParts.Add(ColumnHelper.SetPreviousSectionToColumns(1, 200));
 
-            returnMe.Add(ColumnHelper.SetPreviousSectionToColumns(1));
-            returnMe.Add(ColumnHelper.MarkSectionStart());
-               
-            // Add left table
-            returnMe.AddRange(
-                BoolTable.MakeTable(new List<KeyValuePair<string, bool>>() {
+            sectionParts.Add(
+                TableHelper.MakeTable(new List<KeyValuePair<string, bool>>() {
                     new KeyValuePair<string, bool>("KidsFirst", PreKInfo.AssistanceFromKidsFirst),
-                    new KeyValuePair<string, bool>("Early Childhood Intervention", PreKInfo.AssistanceFromEarlyChildhoodIntervention),
-                    new KeyValuePair<string, bool>("Occupational or Physical Therapist", PreKInfo.AssistanceFromOccupationOrPhysicalTherapist),
-                    new KeyValuePair<string, bool>("Childhood Psychologist", PreKInfo.AssistanceFromChildhoodPsychologist),
-                    new KeyValuePair<string, bool>("Pre-School or Daycare", PreKInfo.AssistanceFromPreSchoolOrDaycare),
+                    new KeyValuePair<string, bool>("Early Childhood Intervention Program", PreKInfo.AssistanceFromEarlyChildhoodIntervention),
+                    new KeyValuePair<string, bool>("Occupational/Physical Therapist", PreKInfo.AssistanceFromOccupationOrPhysicalTherapist),
+                    new KeyValuePair<string, bool>("Early Childhood Psychologist", PreKInfo.AssistanceFromChildhoodPsychologist),
+                    new KeyValuePair<string, bool>("Pre-School/Daycare/Family Day Home", PreKInfo.AssistanceFromPreSchoolOrDaycare),
                     new KeyValuePair<string, bool>("Licensed Child Care", PreKInfo.AssistanceFromKidsFirst),
-                }));
-
-            // Add column break between the tables
-            returnMe.Add(new Paragraph(new Run(new Break() { Type = BreakValues.Column })));
-
-            // Add right table
-            returnMe.AddRange(
-                BoolTable.MakeTable(new List<KeyValuePair<string, bool>>() {
-                    new KeyValuePair<string, bool>("Autism Consultant", PreKInfo.AssistanceFromEarlyChildhoodIntervention),
-                    new KeyValuePair<string, bool>("Speech Language Pathologist", PreKInfo.AssistanceFromOccupationOrPhysicalTherapist),
+                    new KeyValuePair<string, bool>("Autism Consultant or Resource Center", PreKInfo.AssistanceFromEarlyChildhoodIntervention),
+                    new KeyValuePair<string, bool>("Speech/Language Pathologist", PreKInfo.AssistanceFromOccupationOrPhysicalTherapist),
                     new KeyValuePair<string, bool>("Social Services", PreKInfo.AssistanceFromChildhoodPsychologist),
-                    new KeyValuePair<string, bool>("Kinsmen Child Dev Cntr", PreKInfo.AssistanceFromPreSchoolOrDaycare),
-                    new KeyValuePair<string, bool>("Aboriginal Headstart", PreKInfo.AssistanceFromKidsFirst)
+                    new KeyValuePair<string, bool>("Kinsmen Child Development Center", PreKInfo.AssistanceFromPreSchoolOrDaycare),
+                    new KeyValuePair<string, bool>("Aboriginal HeadStart", PreKInfo.AssistanceFromKidsFirst),
+                    new KeyValuePair<string, bool>("Consent to share information with these agencies", PreKInfo.ConsentToShareFromAgencies)
                 }));
             
             // Make this section have 2 columns            
-            returnMe.Add(ColumnHelper.SetPreviousSectionToColumns(2, 200));
-            returnMe.Add(ColumnHelper.MarkSectionStart());
-            return returnMe;                
+            sectionParts.Add(ColumnHelper.SetPreviousSectionToColumns(2, 200));
+            return sectionParts;
         }
 
     }
