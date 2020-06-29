5ba87e6bf22bbae85b06ac4a7336c8d57d5352b3
Mark Strendin <mark.strendin@lskysd.ca>
2020-06-29 12:16:20 -0600

    Add some clarification to the email message sent. (#120)

--- LSSD.Registration.NotificationHandlers.EmailNotificationHandler/EmailNotifications/PreKEmailNotification.cs
+++ LSSD.Registration.NotificationHandlers.EmailNotificationHandler/EmailNotifications/PreKEmailNotification.cs
@@ -32,6 +32,7 @@ namespace LSSD.Registration.NotificationHandlers.EmailNotificationHandler
                         <h3>Pre-K Application for " + _form.Form.Student.LegalFirstName + " " + _form.Form.Student.LegalLastName + @"</h3>
                         <p>Attached is a pre-kindergarten application for " + _form.Form.Student.LegalFirstName + " " + _form.Form.Student.LegalLastName + @", 
                         submitted " + _form.DateReceived(_timeZone).ToLongDateString() + " at " + _form.DateReceived(_timeZone).ToShortTimeString() + @"</p> 
+                        <p>The online registration system is open to anyone on the Internet, and so it is possible to send fake or abusive registrations. If you believe the attached file to be fake or abusive, it is safe to delete and forget about. All forms have a unique ID associated with them, which is printed on the form itself - the form attached to this email has the ID: " + _form.Id + @". </p>
                         <p>If you have trouble opening this file, please create a Help Desk Ticket at https://helpdesk.lskysd.ca.</p>
                         <p>Note that parents may select up to three school choices, and all three schools are sent this email.</p>
                         <p>The attached file may contain private personal information - please consider this when forwarding this email message.</p>
