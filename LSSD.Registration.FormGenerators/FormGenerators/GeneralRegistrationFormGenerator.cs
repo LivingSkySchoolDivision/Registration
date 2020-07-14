5e5817782255d6a7839ff6fb3526c2802e4f91d3
Mark Strendin <mark.strendin@lskysd.ca>
2020-07-14 16:09:21 -0600

    Add student cell phone to student demographic page (#122)

--- LSSD.Registration.FormGenerators/FormGenerators/GeneralRegistrationFormGenerator.cs
+++ LSSD.Registration.FormGenerators/FormGenerators/GeneralRegistrationFormGenerator.cs
@@ -48,7 +48,6 @@ namespace LSSD.Registration.FormGenerators.FormGenerators {
             pageParts.AddRange(EALSection.GetSection(Form.Form.EALInfo));
             pageParts.AddRange(CitizenshipSection.GetSection(Form.Form.Citizenship));
             pageParts.AddRange(FirstNationsSection.GetSection(Form.Form.FirstNationsInfo));
-            pageParts.Add(ParagraphHelper.PageBreak());  
             pageParts.AddRange(ContactsSection.GetSection(Form.Form.Contacts));   
             pageParts.AddRange(FormEndSection.GetSection(Form, TimeZone)); 
             return new Document(new Body(pageParts));
